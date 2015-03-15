ab68a5abeba7c78c11e4cca8b94e55695f9a30b6
Jason Xu <jason.xry@gmail.com>
2015-03-15 13:08:21 +0800

    Dynamic framework

---
+++ CurriedFunctions/CurriedFunctions.h
@@ -0,0 +1,19 @@
+//
+//  CurriedFunctions.h
+//  CurriedFunctions
+//
+//  Created by Jason Xu on 3/15/15.
+//  Copyright (c) 2015 Jason Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CurriedFunctions.
+FOUNDATION_EXPORT double CurriedFunctionsVersionNumber;
+
+//! Project version string for CurriedFunctions.
+FOUNDATION_EXPORT const unsigned char CurriedFunctionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurriedFunctions/PublicHeader.h>
+
+
